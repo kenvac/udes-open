eb12a735dec35badc1d343cad76fd28636ac357a
Robert Smith <robert.smith@unipart.io>
2020-10-12 13:32:49 +0100

    [ADD] udes_warehouse_classification: Extend _prepare_info
    Extending _prepare_info to include classification messages for a
    given picking
    
    User-story: 11592
    Signed-off-by: Robert Smith <robert.smith@unipart.io>

--- addons/udes_warehouse_classification/models/stock_picking.py
+++ addons/udes_warehouse_classification/models/stock_picking.py
@@ -10,4 +10,14 @@ class StockPicking(models.Model):
         """Find product classifications needed for a given picking"""
         return self.mapped("move_lines.product_id.u_product_warehouse_classification_ids").filtered(
             lambda c: self.picking_type_id in c.picking_type_ids
-        )
+        ).mapped("alert_message")
+
+    def _prepare_info(self, priorities=None, fields_to_fetch=None):
+        """{doc}
+           Extensions:
+           - u_classification_messages: list
+        """.format(doc=super()._prepare_info.__doc__)
+        info = super()._prepare_info(priorities=priorities, fields_to_fetch=fields_to_fetch)
+        if fields_to_fetch is None or "u_classification_messages" in fields_to_fetch:
+            info["u_classification_messages"] = self._get_product_classfication_messages()
+        return info
